70bf08183cd2c855d166287c66386a9a09ab9b89
Felipe Moreira <fmoreira95@hotmail.com>
2021-07-29 12:24:11 -0400

    updated click events to three mobile buttons, making axios call to data.json

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -4,6 +4,7 @@ const mobileName = document.querySelector(".planet-name");
 const mobileDesc = document.querySelector(".planet-description");
 const mobileSource = document.querySelector(".wiki-source");
 const mobileImg = document.querySelector(".planet-img");
+const mobileGeoImg = document.querySelector(".planet-geoImg");
 
 let data = "../data/data.json";
 // render content with click event
@@ -23,6 +24,11 @@ mobileBtns.forEach((btn) => {
       mobileDesc.textContent = json[cursor][option].content;
       mobileSource.href = json[cursor][option].source;
       mobileImg.src = json[cursor].images[picture];
+      mobileGeoImg.src = json[cursor].images.geology;
+      mobileGeoImg.classList.remove("geology");
+      if (btn.dataset.option == "geology") {
+        mobileGeoImg.classList.add("geology");
+      }
     });
   });
 });
